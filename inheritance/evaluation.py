1ceb4fbcf1113af205ea962bf1fbb55fa9839c9d
Muhammad Salman Shahid <muhammad-salman.shahid@grenoble-inp.fr>
2026-02-09 14:05:14 +0100

    inheritance : evaluation

---
+++ inheritance/evaluation.py
@@ -0,0 +1,24 @@
+class CelestialBody:
+    def __init__(self, size, mass, composition, name):
+        self.size = size
+        self.mass = mass
+        self.composition = composition
+        self.name = name
+
+
+# create the satellite class
+class Satellite(CelestialBody):
+    def __init__(self, size, mass, composition, name, host_planet):
+        super().__init__(size, mass, composition, name)
+        self.host_planet = host_planet
+
+
+# create the planet class
+class Planet(CelestialBody):
+    def __init__(self, size, mass, composition, name, host_star):
+        super().__init__(size, mass, composition, name)
+        self.host_star = host_star
+
+
+p = Planet(1, 2, "round", "earth", "moon")
+print(p.host_star)
